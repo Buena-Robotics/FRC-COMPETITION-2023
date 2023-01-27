9ebcd8eca8a8a17db2a48972de4715fd7fba63e8
Daniel Raygoza <68447327+Primal-Illusion@users.noreply.github.com>
2023-01-27 15:54:11 -0700

    base

---
+++ src/main/java/frc/robot/commands/DriveJoystickCommand.java
@@ -0,0 +1,29 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.DrivetrainSubsystem;
+
+public class DriveJoystickCommand extends CommandBase{
+    private final DrivetrainSubsystem m_subsystem;
+
+    public DriveJoystickCommand(DrivetrainSubsystem subsystem) {
+        m_subsystem = subsystem;
+        // Use addRequirements() here to declare subsystem dependencies.
+        addRequirements(subsystem);
+    }
+
+    @Override
+    public void initialize() {}
+
+    @Override
+    public void execute() {}
+
+    @Override
+    public void end(boolean interrupted) {}
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
