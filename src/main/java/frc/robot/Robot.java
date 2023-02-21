7e4fadcc09370c67f93d790b8c39772d837606ea
BadGamesInc <ibadgames5000@gmail.com>
2023-02-21 15:37:08 -0700

    Clean up stuff

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,8 +8,6 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.commands.DriveDistanceCommand;
 import frc.robot.nerds.utils.ControllerUtils;
 import frc.robot.nerds.utils.GamePositionUtils;
 
